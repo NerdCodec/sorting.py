813ca8985148b6830edfc4151cf90275b18a0aae
Dante <151466105+NerdCodec@users.noreply.github.com>
2024-11-22 03:33:03 +0300

    Add files via upload

---
+++ Sorting.py
@@ -0,0 +1,7 @@
+SixIntegers=[]
+num=eval(input("Enter a number:"))
+SixIntegers.append(num)
+if (len(SixIntegers) == 6):
+    print('No input is needed')
+else:
+    num=input('Continue Entering a number:')   
